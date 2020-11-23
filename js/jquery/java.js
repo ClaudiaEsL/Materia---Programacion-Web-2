e8c1198f1979e1509da0f67f7190dca092980cbb
brian <nsklobuchar666@gmail.com>
2020-11-23 16:36:39 -0400

    cards de inisiar sesion listo

---
+++ js/jquery/java.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+/* Ventana Incisiar sesion y registro*/
+
+$("#uno").click(function () { 
+   
+    $("#inicio-sesion").css({
+        "display":"block"
+    })
+    
+});
+
+
+
+
+/* cancelar o cerrar ventana */
+$("#salir").click(function () { 
+    $("#inicio-sesion").css({"display":"none"})
+    
+});
